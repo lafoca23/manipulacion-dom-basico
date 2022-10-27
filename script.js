b6544cb48b6e19c25047e0ec8a5c5e5e20fd18a9
lafoca23 <lafocaoficial23@gmail.com>
2022-10-27 12:10:48 -0500

    Agregar .gitignore y primeros archivos del proyecto

---
+++ script.js
@@ -0,0 +1,20 @@
+const h1 = document.querySelector('h1');
+const form = document.querySelector('#formulario')
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelector('#btnCalcular');
+const pResult = document.querySelector('#result');
+
+form.addEventListener('click', sumarImputValues);
+
+// function sumarImputValues(event) {
+//    console.log({event})
+//    event.preventDefault();
+//    const sumaInputs = parseInt(input1.value) + parseInt(input2.value);
+//    pResult.innerText = "Resultado: " + sumaInputs;
+// }  
+
+function sumarImputValues(event) {
+   const sumaInputs = parseInt(input1.value) + parseInt(input2.value);
+   pResult.innerText = "Resultado: " + sumaInputs;
+}  
